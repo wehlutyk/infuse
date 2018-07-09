43cb6e2d0d85f90d85276cd66af52f8d35c34f17
Sébastien Lerique <sl@mehho.net>
2018-07-09 16:57:18 +0200

    feat/all: use log and env_logger

--- src/lib.rs
+++ src/lib.rs
@@ -3,13 +3,13 @@
 extern crate wasm_bindgen;
 extern crate console_error_panic_hook;
 extern crate lopdf;
+#[macro_use]
+extern crate log;
 
 #[cfg(all(target_arch = "wasm32", not(target_os = "emscripten")))]
 use std::panic;
 use std::io::Cursor;
-
 use wasm_bindgen::prelude::*;
-
 use lopdf::Document;
 
 #[cfg(all(target_arch = "wasm32", not(target_os = "emscripten")))]
@@ -19,10 +19,26 @@ extern {
     fn log(s: &str);
 }
 
-#[cfg(not(all(target_arch = "wasm32", not(target_os = "emscripten"))))]
-fn log(s: &str) {
-    println!("{}", s);
+#[cfg(all(target_arch = "wasm32", not(target_os = "emscripten")))]
+struct WasmConsoleLogger;
+
+#[cfg(all(target_arch = "wasm32", not(target_os = "emscripten")))]
+impl log::Log for WasmConsoleLogger {
+    fn enabled(&self, metadata: &log::Metadata) -> bool {
+        metadata.level() <= log::Level::Trace
+    }
+
+    fn log(&self, record: &log::Record) {
+        if self.enabled(record.metadata()) {
+            log(&format!("{} - {}", record.level(), record.args()));
+        }
+    }
+
+    fn flush(&self) {}
 }
+
+#[cfg(all(target_arch = "wasm32", not(target_os = "emscripten")))]
+static WASM_LOGGER: WasmConsoleLogger = WasmConsoleLogger;
 
 #[wasm_bindgen]
 pub fn init() {
@@ -31,22 +47,24 @@ pub fn init() {
     #[cfg(all(target_arch = "wasm32", not(target_os = "emscripten")))]
     {
         panic::set_hook(Box::new(console_error_panic_hook::hook));
+        log::set_logger(&WASM_LOGGER)
+            .map(|()| log::set_max_level(log::LevelFilter::Trace)).unwrap();
         prefix = "Wasm";
     }
     #[cfg(not(all(target_arch = "wasm32", not(target_os = "emscripten"))))]
     {
         prefix = "Non-wasm";
     }
-    log(&format!("{} infuse initialised", &prefix));
+    info!("{} infuse initialised", &prefix);
 }
 
 #[wasm_bindgen]
 pub fn process_file_data(data: Vec<u8>) {
-    log(&format!("Processing file (size = {}b)", data.len()));
+    info!("Processing file (size = {}b)", data.len());
 
     let cursor = Cursor::new(data);
     let doc = Document::load_from(cursor).unwrap();
 
-    log(&format!("PDF version: {}", doc.version));
-    log(&format!("#pages: {}", doc.get_pages().len()));
+    info!("PDF version: {}", doc.version);
+    info!("#pages: {}", doc.get_pages().len());
 }
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,6 @@
+#[macro_use]
+extern crate log;
+extern crate env_logger;
 extern crate infuse;
 
 use std::env;
@@ -6,17 +9,28 @@ use std::io::BufReader;
 use std::io::prelude::*;
 
 fn main() -> std::io::Result<()> {
+    // Set up logging
+    let mut builder = env_logger::Builder::new();
+    builder.filter_level(log::LevelFilter::Trace);
+
+    if env::var("RUST_LOG").is_ok() {
+       builder.parse(&env::var("RUST_LOG").unwrap());
+    }
+
+    // Initialise both logging and infuse
+    builder.init();
     infuse::init();
 
+    // Process arguments
     let arguments = env::args().skip(1);
-    println!("[args] {} file(s)", arguments.len());
+    info!("[args] {} file(s)", arguments.len());
     for argument in arguments {
-        println!("[processing] file name = {}", &argument);
+        info!("[processing] file name = {}", &argument);
         let file = File::open(&argument)?;
         let mut buf_reader = BufReader::new(file);
         let mut contents: Vec<u8> = vec![];
         buf_reader.read_to_end(&mut contents)?;
-        println!("[processing] reading \"{}\" ended", &argument);
+        info!("[processing] reading \"{}\" ended", &argument);
 
         infuse::process_file_data(contents);
     }
