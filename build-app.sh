9a8f3a50f4cde295f04b4c755d0bcb5d62cb937d
Sébastien Lerique <sl@mehho.net>
2018-07-14 22:44:54 +0200

    style/build: logging style

--- build-app.sh
+++ build-app.sh
@@ -3,4 +3,8 @@ echo "Building debug web targets..."
 echo
 rm -f app/infuse.d.ts app/infuse.js app/infuse_bg.wasm
 cargo build --target=wasm32-unknown-unknown
+
+echo
+echo -e "\033[1;32m  Processing\033[0m running wasm-bindgen"
 wasm-bindgen target/wasm32-unknown-unknown/debug/infuse.wasm --out-dir app
+echo -e "\033[1;32m    Finished\033[0m wasm-bindgen"
